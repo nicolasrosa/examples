d7bbd4082497c08b2881aa8efa90919450a0bf06
nicolasrosa <nicolas.rosa@hotmail.com>
2024-03-22 21:03:59 -0300

    [Mod] run_multinode_radagast.sh working!

--- distributed/ddp-tutorial-series/run_multinode_radagast.sh
+++ distributed/ddp-tutorial-series/run_multinode_radagast.sh
@@ -7,12 +7,17 @@ export NODE=0
 # ---
 
 # --- Local Network (Direct connection)
+#export IP=10.235.0.9
+#export IP=10.235.3.46
+#export IP=143.107.235.101
 export IP=192.168.0.1  # Radagast (master)
 #export IP=192.168.0.2  # Iluvatar (slave)
 # ---
 
 #export PORT=29503  # nmap: closed
-export PORT=6818  # nmap: open
+#export PORT=7070 # nmap: open
+export PORT=6817  # nmap: open
+#export PORT=24900 # nmap: open
 # ---
 
 # --- Print Environment Variables
@@ -23,7 +28,9 @@ echo "---"
 # ---
 
 # Following commands are failing
-# torchrun --nproc_per_node=2 --nnodes=2 --node_rank=1 --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=${IP}:${PORT} multinode.py 50 10
+#torchrun --nproc_per_node=2 --nnodes=2 --node_rank=${NODE} --rdzv_id=456 --rdzv_backend=c10d --rdzv_endpoint=${IP}:${PORT} multinode.py 50 10
 
 # For some reason don't specify the rdzv_backend seems to start to work
-torchrun --nproc_per_node=2 --nnodes=2 --node_rank=1 --rdzv_id=456 --rdzv_endpoint=${IP}:${PORT} multinode.py 50 10
+#NCCL_DEBUG=INFO \
+NCCL_SOCKET_IFNAME=enx5ce9315b5950 \  # Radagast's USB Ethernet Interface name
+torchrun --nproc_per_node=2 --nnodes=2 --node_rank=${NODE} --rdzv_id=456 --rdzv_endpoint=${IP}:${PORT} multinode.py 50 10
